8878526320cdcd73c49bfd472225c8dbad249b16
Самотрясов Роман Русланович <rsamotryasov@t1-consulting.ru>
2022-02-12 17:01:45 +0300

    Установил express, настроил сервер

---
+++ server/server.js
@@ -0,0 +1,13 @@
+// import express from "express";
+
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("./static"));
+
+app.listen(port, () => {
+  console.log("Server running on port 3000");
+});
