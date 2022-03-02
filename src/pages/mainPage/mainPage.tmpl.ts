ade28d61e2a1529cdad258dff83216fd787021d1
Самотрясов Роман Русланович <rsamotryasov@t1-consulting.ru>
2022-03-02 21:39:57 +0300

    mainPage переписан на компонент

--- src/pages/mainPage/mainPage.tmpl.ts
+++ src/pages/mainPage/mainPage.tmpl.ts
@@ -1,11 +1,9 @@
 const template = `
-<main class="main-page">
-    <section class="main-page__content">
-    <a href="./loginPage.html" class="button">Войти</a>
+<main class={{mainClassName}}>
+    <section class={{sectionClassName}}>
+    {{signInLink}}
     <p class="main-page__text">или</p>
-    <a href="./signUpPage.html" class="button button--underline"
-        >Зарегистрироваться</a
-    >
+    {{signUpLink}}
     </section>
 </main>
 `;
--- src/pages/mainPage/mainPage.ts
+++ src/pages/mainPage/mainPage.ts
@@ -1,12 +1,42 @@
-import {Templator} from '../../utils';
+import Block from '../../components/block/Block';
+import {Link} from '../../components/link';
+import {renderDOM} from '../../utils';
 import mainPageTemplate from './mainPage.tmpl';
 
-const template = new Templator(mainPageTemplate);
+type MainPageProps = {
+  mainClassName: string;
+  sectionClassName: string;
+  signInLink: Link;
+  signUpLink: Link;
+};
 
-const compiled = template.compile();
+class MainPage extends Block {
+  constructor(props: MainPageProps) {
+    super('div', props);
+  }
+
+  render() {
+    return this.compile(mainPageTemplate, this.props);
+  }
+}
+
+const props = {
+  mainClassName: '"main-page"',
+  sectionClassName: '"main-page__content"',
+  signInLink: new Link({
+    to: './loginPage.html',
+    className: 'button',
+    text: 'Войти',
+  }),
+  signUpLink: new Link({
+    to: './signUpPage.html',
+    className: '"button button--underline"',
+    text: 'Зарегистрироваться',
+  }),
+};
+
+const page = new MainPage(props);
 
 const rootDiv = document.getElementById('root');
 
-if (rootDiv) {
-  rootDiv.innerHTML = compiled;
-}
+renderDOM(rootDiv, page);
