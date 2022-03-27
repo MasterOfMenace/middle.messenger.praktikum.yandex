43e8cc211a6c9bce75b6d72294810adce8658ab4
Самотрясов Роман Русланович <rsamotryasov@t1-consulting.ru>
2022-03-27 13:24:04 +0300

    Тесты для httpTransport и templator

---
+++ src/utils/httpTransport.test.ts
@@ -0,0 +1,34 @@
+import {expect} from 'chai';
+import HTTPTransport from './httpTransport';
+
+const httpTransport = new HTTPTransport('http://fakeurl.com');
+
+describe('HTTP transport method existing tests', () => {
+  it('should have a method get', () => {
+    expect(httpTransport).to.have.property('get');
+  });
+  it('should have a method post', () => {
+    expect(httpTransport).to.have.property('post');
+  });
+  it('should have a method put', () => {
+    expect(httpTransport).to.have.property('put');
+  });
+  it('should have a method delete', () => {
+    expect(httpTransport).to.have.property('delete');
+  });
+});
+
+describe('HTTP transport work tests', () => {
+  it('method get should return a promise', () => {
+    expect(httpTransport.get('/someurl')).to.be.an.instanceOf(Promise);
+  });
+  it('method post should return a promise', () => {
+    expect(httpTransport.post('/someurl')).to.be.an.instanceOf(Promise);
+  });
+  it('method put should return a promise', () => {
+    expect(httpTransport.put('/someurl')).to.be.an.instanceOf(Promise);
+  });
+  it('method delete should return a promise', () => {
+    expect(httpTransport.delete('/someurl')).to.be.an.instanceOf(Promise);
+  });
+});
--- src/utils/templator.test.ts
+++ src/utils/templator.test.ts
@@ -1,25 +1,52 @@
 import {expect} from 'chai';
 import Templator from './templator';
 
-const template = `
-  <div class="{{className}}">
-    {{text}}
-  </div>`;
-
-const context = {
-  className: 'my-class',
-  text: 'Hello World',
-};
-
-const element = new Templator(template).compile(context);
-
 describe('Templator base test', () => {
-  it('should return compiled template correctly', () => {
-    expect(
-      element,
-      `<div class="my-class">
-    Hello World
-  </div>`,
-    );
+  it('should return simple compiled template correctly', () => {
+    const template = `
+    <div class="{{className}}">
+      {{text}}
+    </div>`;
+
+    const expectResult = `
+    <div class="my-class">
+      Hello World
+    </div>`;
+
+    const context = {
+      className: 'my-class',
+      text: 'Hello World',
+    };
+
+    expect(new Templator(template).compile(context)).to.equal(expectResult);
+  });
+
+  it('should return nested compiled template correctly', () => {
+    const template = `
+    <div class="{{className}}">
+      <p>{{text}}</p>
+      <div class="{{nestedElem.className}}">
+        <span>{{nestedElem.text}}</span>
+      </div>
+    </div>`;
+
+    const expectedResult = `
+    <div class="my-class">
+      <p>Hello World</p>
+      <div class="my-nested-class">
+        <span>Nested Text</span>
+      </div>
+    </div>`;
+
+    const context = {
+      className: 'my-class',
+      text: 'Hello World',
+      nestedElem: {
+        className: 'my-nested-class',
+        text: 'Nested Text',
+      },
+    };
+
+    expect(new Templator(template).compile(context)).to.equal(expectedResult);
   });
 });
